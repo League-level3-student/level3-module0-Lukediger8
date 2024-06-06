0631cfa32baa814c6e4b06a92c9821cfee007df6
league <league@192.168.1.135>
2024-06-06 10:55:55 -0700

    finished

--- src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
+++ src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
@@ -50,26 +50,89 @@ package _07_The_Wrong_Way_Cow;
 
 public class TheWrongWayCow {
 
-    public static int[] findWrongWayCow(final char[][] field) {
-        // Fill in the code to return the [col, row] coordinate position of the
-        // head (letter 'c') of the wrong way cow!
-    	int forward = 0;
-    	int backward = 0;
-    	int up = 0;
-    	int down = 0;
-    	
-    	
-for(int i = 0; i < field.length; i++) {
-	for(int j = 0; j < field[i].length - 2;i++) {
-		if(field[i][j] == 'c') {
-			if(field[i][j + 1]=='o') {
-				if(field[i][j + 2] == 'w') {
-					forward++;
+	public static int[] findWrongWayCow(final char[][] field) {
+		// Fill in the code to return the [col, row] coordinate position of the
+		// head (letter 'c') of the wrong way cow!
+		int forward = 0;
+		int backward = 0;
+		int up = 0;
+		int down = 0;
+		int[] forwardArray = { -1, -1 };
+		int[] backwardArray = { -1, -1 };
+		int[] upArray = { -1, -1 };
+		int[] downArray = { -1, -1 };
+
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[i].length - 2; j++) {
+				if (field[i][j] == 'c') {
+					if (field[i][j + 1] == 'o') {
+						if (field[i][j + 2] == 'w') {
+							forward++;
+							forwardArray[0] = j;
+							forwardArray[1] = i;
+						}
+					}
 				}
 			}
 		}
+
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[i].length - 2; j++) {
+				if (field[i][j] == 'w') {
+					if (field[i][j + 1] == 'o') {
+						if (field[i][j + 2] == 'c') {
+							backward++;
+							backwardArray[0] = j + 2;
+							backwardArray[1] = i;
+
+						}
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < field[0].length; i++) {
+			for (int j = 0; j < field.length - 2; j++) {
+				if (field[j][i] == 'w') {
+					if (field[j + 1][i] == 'o') {
+						if (field[j + 2][i] == 'c') {
+							up++;
+							upArray[0] = i;
+							upArray[1] = j + 2;
+
+						}
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < field[0].length; i++) {
+			for (int j = 0; j < field.length - 2; j++) {
+				if (field[j][i] == 'c') {
+					if (field[j + 1][i] == 'o') {
+						if (field[j + 2][i] == 'w') {
+							down++;
+							downArray[0] = i;
+							downArray[1] = j;
+
+						}
+					}
+				}
+			}
+		}
+		if (forward == 1) {
+			return forwardArray;
+
+		}
+		if (backward == 1) {
+			return backwardArray;
+		}
+		if (up == 1) {
+			return upArray;
+		}
+		if(down == 1) {
+		return downArray;
+		}
+		return upArray;
 	}
-}
-        return null;
-    }
 }
